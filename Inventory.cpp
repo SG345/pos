ea028bf7733b7195b153886af81b936fd2abcdba
Sushrut Girdhari <SG345@users.noreply.github.com>
2017-06-24 23:39:23 +0530

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,14 +1,14 @@
 #include "inventory.h"
 
 Inventory::Inventory(string pname, string pd, double p, int qty) {
-	
+
 	product_name = pname;
 	product_description = pd;
 	quantity = qty;
 	price = p;
 }
 
-void Inventory::sale_request(int qty){
-	
+void Inventory::sale_request(int qty) {
+
 	this->quantity = this->quantity - qty;
 }
