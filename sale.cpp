c3c8ee18dd1bf62484518a0362700a2872a35448
Sushrut Girdhari <SG345@users.noreply.github.com>
2017-06-24 23:40:57 +0530

    Update sale.cpp

--- sale.cpp
+++ sale.cpp
@@ -2,47 +2,47 @@
 #include "inventory.h"
 
 Sale::Sale(string name) {
-		username=name;
-		totalbill = 0.0;
-	}
+	username = name;
+	totalbill = 0.0;
+}
 
-bool Sale::isvalid( Inventory& I,int qty) {
+bool Sale::isvalid( Inventory& I, int qty) {
 
-	if (qty <= I.quantity && qty>0)
+	if (qty <= I.quantity && qty > 0)
 		return true;
 	return false;
 }
 
-void Sale::addcart(int pid, vector<Inventory>& I, int Q){
+void Sale::addcart(int pid, vector<Inventory>& I, int Q) {
 
-	I[pid].sale_request(Q); //update inventory with current sale. 
-	Bill.push_back(Inventory(I[pid].product_name,I[pid].product_description,(double)I[pid].price,Q)); //update bill
+	I[pid].sale_request(Q); //update inventory with current sale.
+	Bill.push_back(Inventory(I[pid].product_name, I[pid].product_description, (double)I[pid].price, Q)); //update bill
 }
 
-void Sale::computebill(){
+void Sale::computebill() {
 
-	for(int i = 0; i < this->Bill.size(); i++) {
-		(this->totalbill)+=(Bill[i].price*Bill[i].quantity);
+	for (int i = 0; i < this->Bill.size(); i++) {
+		(this->totalbill) += (Bill[i].price * Bill[i].quantity);
 	}
 }
 
-void Sale::printbill(){
-	
-	cout<<"-------------BILL FOR POS SALE -----------"<<endl<<endl;;
-	cout<<"Product Name:\t  "<<"Quantity\t"<<"Price per unit\t"<<endl;
+void Sale::printbill() {
 
-	for(int i = 0; i < this->Bill.size(); i++){
-		cout<<this->Bill[i].product_name<<"\t"<<this->Bill[i].quantity<<"\t"<<" Rs."<<this->Bill[i].price<<endl;
+	cout << "-------------BILL FOR POS SALE -----------" << endl << endl;;
+	cout << "Product Name:\t  " << "Quantity\t" << "Price per unit\t" << endl;
+
+	for (int i = 0; i < this->Bill.size(); i++) {
+		cout << this->Bill[i].product_name << "\t" << this->Bill[i].quantity << "\t" << " Rs." << this->Bill[i].price << endl;
 	}
 
-	cout<<"Total Bill Amount: \t"<<"Rs. "<<this->totalbill<<endl;
-	cout<<"Thanks for transacting with us, "<<this->username<<" "<<"Hope to see you soon";
-	cout<<endl;
-    cout<<"-----------------------------------------"<<endl;
+	cout << "Total Bill Amount: \t" << "Rs. " << this->totalbill << endl;
+	cout << "Thanks for transacting with us, " << this->username << " " << "Hope to see you soon";
+	cout << endl;
+	cout << "-----------------------------------------" << endl;
 
 }
 
-double Sale::GetBillAmount(){
+double Sale::GetBillAmount() {
 
 	return this->totalbill;
 }
